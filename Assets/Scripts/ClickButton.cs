802df541fdcc14e2d689176cf5586076874e4f4b
Jarryd Huntley <jarryd.huntley@gmail.com>
2015-01-24 04:06:07 -0500

    Addded in the color change after clicking the button.

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickButton : MonoBehaviour {
+
+	public Material changeTo;
+	public ButtonManager mainButtonScript;
+
+	// Use this for initialization
+	void Start () {
+	
+		GameObject manager = GameObject.Find("Button Manager Object");
+		mainButtonScript = manager.GetComponent<ButtonManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				// the object identified by hit.transform was clicked
+				// do whatever you want
+				Debug.Log("Touched something.");
+				//Debug.DrawLine(ray.origin, hit.point);
+
+				hit.collider.renderer.material = changeTo;
+
+			}
+		}
+
+
+
+	}
+}
