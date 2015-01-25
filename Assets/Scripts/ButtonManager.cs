d5e078a9629e65f8bb062160b837b0b5bdf089c8
Jarryd Huntley <jarryd.huntley@gmail.com>
2015-01-25 05:40:01 -0500

    Game logic DONE! Now on to finish designing and polish!

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -22,12 +22,12 @@ public class ButtonManager : MonoBehaviour {
 	private bool lose = false;
 	//public bool pressed = false;
 
-	private bool showTimer = false;
+	private bool showTimer ;
 
 	// Use this for initialization
 	void Start () {
 
-
+		showTimer = false;
 		buttonList = GameObject.FindObjectsOfType<ButtonScript> ();
 
 		Shuffle (buttonList);
@@ -46,16 +46,18 @@ public class ButtonManager : MonoBehaviour {
 
 		messageTimer-=Time.deltaTime;
 
-		if(showTimer)
-		levelTimer -=Time.deltaTime;
-
+		if (showTimer) {
+			levelTimer -= Time.deltaTime;
+		}
 
 		if((messageTimer> -5  && messageTimer < 0 )&& !hasCalledFirst && !win) {
 			
 			setMessage("Please find the "+ buttonList[counter].returnMatertialName() + "" + buttonList[counter].description+"!",30);
 			hasCalledFirst = true;
-		}
 
+			showTimer = true;
+		}
+		//Debug.Log (showTimer);
 
 		if(messageTimer < 0 && win && !lose) {
 			Debug.Log("Game end.");
@@ -68,7 +70,7 @@ public class ButtonManager : MonoBehaviour {
 			#endif
 		}
 
-		if(levelTimer < 0 ) {
+		if(levelTimer < 0 && !win) {
 			lose = true;
 			setMessage("I'm sorry! Therese nothing we can do to save you now. What do we do now?!",10);
 		}
@@ -130,7 +132,7 @@ public class ButtonManager : MonoBehaviour {
 		
 		}
 
-		if (allPressed && !win){
+		if (allPressed && !win && !lose){
 
 			//setMessage("Good work diverting the submarine! Now lets get you out of there!!",10);
 			setMessage("Good work diverting the submarine! Now lets get you out of there!!",10);
@@ -151,17 +153,20 @@ public class ButtonManager : MonoBehaviour {
 			GUI.Box(new Rect(0, (Screen.height/2)+80, Screen.width/2, Screen.height), myGUIText);
 		}
 
-		int minutes = Mathf.FloorToInt(levelTimer / 60F);
-		int seconds = Mathf.FloorToInt(levelTimer - minutes * 60);
-		
-	
-		if (showTimer) {
-			string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
-			Debug.Log(niceTime);
-			GUI.Box(new Rect(10,10,250,100), niceTime+"");
+		if ((levelTimer > 0)&& showTimer ) {
+			drawTime();
 		}
 	}
 
+	void drawTime() {
+		int minutes = Mathf.FloorToInt(levelTimer / 60F);
+		int seconds = Mathf.FloorToInt(levelTimer - minutes * 60);
+
+		string niceTime = string.Format("Time Left: {0:0}:{1:00}", minutes, seconds);
+		GUI.Box(new Rect(Screen.width-150,10,120,20), niceTime+"");
+
+
+	}
 	public void setMessage(string message, float timeOut){
 		myGUIText = message;
 		messageTimer = timeOut;
@@ -186,8 +191,8 @@ public class ButtonManager : MonoBehaviour {
 	public void setFirstMessage(string message, float timeOut){
 		myGUIText = message;
 		messageTimer = timeOut;
-
-		showTimer = true;
+	
+		Debug.Log ("First message");
 	}
 	
 }
